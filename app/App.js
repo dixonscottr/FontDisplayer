6a6ec3077397473c12db9ddbf1a1ea72503457f9
Scott Dixon <dixon.scottr@gmail.com>
2017-02-02 14:45:34 -0500

    Add footer, links and make sure fonts work

--- app/App.js
+++ app/App.js
@@ -5,30 +5,36 @@ import {
   View,
   Linking,
   AlertIOS,
+  ScrollView,
   TouchableHighlight
 } from 'react-native';
+import fonts from '../fonts';
 
 export default class App extends Component {
 
-    linkToFontPage = () => {
-      Linking.canOpenURL('https://github.com/dabit3/react-native-fonts').then(supported => {
-          if (supported) {
-            Linking.openURL('https://github.com/dabit3/react-native-fonts');
-          } else {
-            AlertIOS.alert('Error opening: ' + this.props.url);
-          }
-        });
-    };
+  constructor() {
+    super()
+  }
 
-    linkToGitHub = () => {
-      Linking.canOpenURL('https://github.com/dixonscottr').then(supported => {
+  linkToFontPage = () => {
+    Linking.canOpenURL('https://github.com/dabit3/react-native-fonts').then(supported => {
         if (supported) {
-          Linking.openURL('https://github.com/dixonscottr');
+          Linking.openURL('https://github.com/dabit3/react-native-fonts');
         } else {
           AlertIOS.alert('Error opening: ' + this.props.url);
         }
       });
-    };
+  };
+
+  linkToGitHub = () => {
+    Linking.canOpenURL('https://github.com/dixonscottr').then(supported => {
+      if (supported) {
+        Linking.openURL('https://github.com/dixonscottr');
+      } else {
+        AlertIOS.alert('Error opening: ' + this.props.url);
+      }
+    });
+  };
 
   render() {
     return (
@@ -40,13 +46,21 @@ export default class App extends Component {
           <Text style={styles.instructions}>
             instructions go here
           </Text>
+          <View style={styles.fontHolder}>
+          <ScrollView>
+            {fonts.map((font, i) => {
+                return <Text key={i}>{font}</Text>
+              })
+            }
+          </ScrollView>
+        </View>
         </View>
 
-          <View style={styles.footer}>
-            <Text style={styles.footerText}>Coded by <Text style={styles.link} onPress={this.linkToGitHub}>Scott</Text> | Inspired by
-              <Text style={styles.link} onPress={this.linkToFontPage}> this project</Text>
-            </Text>
-          </View>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Coded by <Text style={styles.link} onPress={this.linkToGitHub}>Scott</Text> | Inspired by
+            <Text style={styles.link} onPress={this.linkToFontPage}> this project</Text>
+          </Text>
+        </View>
 
       </View>
     );
@@ -57,7 +71,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: 'white',
+    backgroundColor: 'white'
   },
   welcome: {
     fontSize: 20,
@@ -71,12 +85,16 @@ const styles = StyleSheet.create({
     color: '#333333'
   },
   main: {
-    flex: 0.95
+    flex: 0.95,
+    overflow: 'hidden'
   },
   footer: {
     flex: 0.05,
     alignItems: 'center'
   },
   footerText: {
+  },
+  fontHolder: {
+    marginTop: 20
   }
 });
